7a4a4e56529cb8f8a557043de3ae9cec40a67d82
unicornGL <s941407@gmail.com>
2024-12-06 02:26:25 +0800

    add models/article

---
+++ lib/models/article.dart
@@ -0,0 +1,42 @@
+class Article {
+  Article({
+    required this.source,
+    required this.author,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.urlToImage,
+    required this.publishedAt,
+    required this.content,
+  });
+
+  final Source? source;
+  final String? author;
+  final String? title;
+  final String? description;
+  final String? url;
+  final String? urlToImage;
+  final String? publishedAt;
+  final String? content;
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    final sourceJson = json['source'];
+    return Article(
+      source: sourceJson != null
+          ? (
+              id: sourceJson['id'],
+              name: sourceJson['name'],
+            )
+          : null,
+      author: json['author'],
+      title: json['title'],
+      description: json['description'],
+      url: json['url'],
+      urlToImage: json['urlToImage'],
+      publishedAt: json['publishedAt'],
+      content: json['content'],
+    );
+  }
+}
+
+typedef Source = ({String? id, String? name});
