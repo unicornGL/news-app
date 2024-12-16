5cfd7d8a78ffa3d9ea249b259a0a495821a9e1c1
unicornGL <s941407@gmail.com>
2024-12-17 02:17:34 +0800

    categories screen temp

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
 
-final List<String> dummyCategories = [
-  'business',
-  'entertainment',
-  'general',
-  'health',
-  'science',
-  'sports',
-  'technology',
-];
+import 'package:intl/intl.dart';
 
-class CategoriesScreen extends StatelessWidget {
+enum Category {
+  business,
+  entertainment,
+  general,
+  health,
+  science,
+  sports,
+  technology,
+}
+
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key});
+
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> {
+  Category? _selectedCategory;
+
+  bool _isSelected(category) => _selectedCategory == category;
+
+  void _selectCategory(Category category) {
+    setState(() {
+      _selectedCategory = _isSelected(category) ? null : category;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,12 +41,36 @@ class CategoriesScreen extends StatelessWidget {
           crossAxisSpacing: 20,
           mainAxisSpacing: 16,
         ),
-        itemCount: dummyCategories.length,
+        itemCount: Category.values.length,
         itemBuilder: (context, index) {
-          return ElevatedButton(
-            onPressed: () {},
+          final category = Category.values[index];
+
+          return OutlinedButton(
+            onPressed: () {
+              _selectCategory(Category.values[index]);
+            },
+            style: OutlinedButton.styleFrom(
+              backgroundColor: _isSelected(category)
+                  ? Theme.of(context).colorScheme.primary
+                  : Colors.transparent,
+              foregroundColor: _isSelected(category)
+                  ? Colors.white
+                  : Theme.of(context).colorScheme.onSurface,
+              side: BorderSide(
+                color: _isSelected(category)
+                    ? Theme.of(context).colorScheme.primary
+                    : Colors.grey.withOpacity(.3),
+                width: 1,
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+            ),
             child: Text(
-              dummyCategories[index],
+              toBeginningOfSentenceCase(category.name),
+              style: TextStyle(
+                fontWeight:
+                    _isSelected(category) ? FontWeight.bold : FontWeight.normal,
+                fontSize: 16,
+              ),
             ),
           );
         },
